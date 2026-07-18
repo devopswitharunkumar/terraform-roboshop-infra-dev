49afb0ee807808e605566c6281c6ff18bdd1e8d0
arunkumar261 <arunkumar261@gmail.com>
2026-07-18 07:11:48 +0530

    udated syntax

--- 02-sg/main.tf
+++ 02-sg/main.tf
@@ -258,6 +258,15 @@ resource "aws_security_group_rule" "catalogue_accept_vpn_traffic" {
   security_group_id        = module.catalogue.sg_id
 }
 
+resource "aws_security_group_rule" "catalogue_accept_vpn_http" {
+  source_security_group_id = module.vpn.sg_id
+  type                     = "ingress"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
+  security_group_id        = module.catalogue.sg_id
+}
+
 # resource "aws_security_group_rule" "catalogue_accept_web_traffic" {
 #   source_security_group_id = module.web.sg_id
 #   type                     = "ingress"
