7f49668fcfe615f5268dcb6db79299654395da5a
arunkumar261 <arunkumar261@gmail.com>
2026-07-19 19:21:56 +0530

    updated web syntax errorcode

---
+++ 09-web/data.tf
@@ -0,0 +1,41 @@
+#fetching ami id from aws
+data "aws_ami" "redhat-9-ami-id" {
+  most_recent      = true
+  owners           = ["973714476881"]
+
+  filter {
+    name   = "name"
+    values = ["Redhat-9-DevOps-Practice"] #if vale randomlyy changes we can give like ["Redhat-9-DevOps-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+data "aws_ssm_parameter" "web_alb_sg_id" {
+  name = "/${var.Project_Name}/${var.Environment}/web_alb_sg_id"
+}
+
+data "aws_ssm_parameter" "private_subnet_id" {
+  name = "/${var.Project_Name}/${var.Environment}/private_subnet_id"
+}
+
+data "aws_ssm_parameter" "vpc_id" {
+  name = "/${var.Project_Name}/${var.Environment}/vpc_id"
+}
+
+
+data "aws_ssm_parameter" "web_sg_id" {
+  name = "/${var.Project_Name}/${var.Environment}/web_sg_id"
+}
+
+data "aws_ssm_parameter" "web_alb_listner_rule" {
+  name = "/${var.Project_Name}/${var.Environment}/web_alb_listner_rule"
+}
---
+++ 09-web/main.tf
@@ -0,0 +1,201 @@
+resource "aws_lb_target_group" "web_targetgroup" {
+  name     = "${var.Project_Name}-${var.Environment}-${var.tags.Component}"
+  port     = 80
+  protocol = "HTTPS"
+  vpc_id   = data.aws_ssm_parameter.vpc_id.value
+  deregistration_delay = 60
+
+  health_check {
+    path = "/health"
+    port = 80
+    healthy_threshold = 2
+    unhealthy_threshold = 3
+    timeout = 5
+    interval = 10
+    matcher = "200-299"  # has to be HTTP 200 or fails
+  }
+}
+
+
+
+#create 1 instance
+module "web" {
+  source = "terraform-aws-modules/ec2-instance/aws"
+  name = "${local.name}-${var.tags.Component}-ami"
+  ami = data.aws_ami.redhat-9-ami-id.id
+  instance_type = "t3.micro"
+  
+  create_security_group = false 
+  vpc_security_group_ids = [data.aws_ssm_parameter.web_sg_id.value]
+  subnet_id = element(split(",", data.aws_ssm_parameter.private_subnet_id.value), 0)
+  iam_instance_profile = "TerraformRoleForEc2"
+
+
+  tags = merge(
+    var.common_tags,
+    var.tags
+  )
+}
+
+
+#provision with ansible
+resource "null_resource" "web" {
+  triggers = {
+    instance_id = module.web.id
+  }
+
+  connection {
+    host = module.web.private_ip
+    type = "ssh"
+    user = "ec2-user"
+    password = "DevOps321"
+  }
+
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh" 
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/bootstrap.sh",
+      "sudo sh /tmp/bootstrap.sh web dev"
+    ]
+  }
+}
+
+
+# stop the instance
+resource "aws_ec2_instance_state" "stop_web" {
+  instance_id = module.web.id
+  state       = "stopped"
+  depends_on = [ null_resource.web ]
+}
+
+# Take the AMI from the stopped instance
+resource "aws_ami_from_instance" "web_ami" {
+  name               = "${local.name}-${var.tags.Component}-AMI-${local.current_date}"
+  source_instance_id = module.web.id
+
+  # Ensures your instance isn't accidentally modified during AMI creation
+  tags = {
+    Name = "${var.tags.Component}-Backup-AMI-For-Roboshop-Proj-AutoScaling"
+  }
+
+  depends_on = [ aws_ec2_instance_state.stop_web ]
+}
+
+#delete/stop(charges apply in stop state) the instance
+resource "null_resource" "web_instance_delete" {
+  triggers = {
+    instance_id = module.web.id
+    # after completing take ami task trigger this null resource
+  }
+
+  provisioner "local-exec" {
+    command = "aws ec2 terminate-instances --instance-ids ${module.web.id}"
+  }
+  depends_on = [ aws_ami_from_instance.web_ami ]
+}
+
+
+#all resources will be created again existing one deleted
+#we have created AMI now, create launch template with AMI
+resource "aws_launch_template" "web_launchtemplate" {
+  name = "${local.name}-${var.tags.Component}-launchtmpt"
+
+
+  image_id = aws_ami_from_instance.web_ami.id
+
+  instance_initiated_shutdown_behavior = "terminate"
+
+  instance_type = "t3.micro"
+  update_default_version = true
+
+#   placement {
+#     availability_zone = "us-east-1a"
+#   }
+
+
+  vpc_security_group_ids = [ data.aws_ssm_parameter.web_sg_id.value ]
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = "${local.name}-${var.tags.Component}-launchtmpt"
+    }
+  }
+}
+
+
+#auto sscaling takes input as launch template
+resource "aws_autoscaling_group" "web_autoscaling" {
+  name                      = "${local.name}-${var.tags.Component}-autoscalinggrp"
+  max_size                  = 10
+  min_size                  = 1
+  health_check_grace_period = 100
+  health_check_type         = "ELB"
+  desired_capacity          = 2
+  
+  vpc_zone_identifier       = split(",", data.aws_ssm_parameter.private_subnet_id.value)
+  target_group_arns = [ aws_lb_target_group.web_targetgroup.arn ]
+  
+
+  launch_template {
+    id      = aws_launch_template.web_launchtemplate.id
+    version = aws_launch_template.web_launchtemplate.latest_version
+  }
+
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      min_healthy_percentage = 50
+    }
+    triggers = ["launch_template"]
+    #launch template epudaithe update avuthundo apudu autoscaling anedi instances refresh avuthai like old instances down ayi new instances u avuthai 
+  }
+  
+
+  tag {
+    key                 = "Name"
+    value               = "${local.name}-${var.tags.Component}"
+    propagate_at_launch = true
+  }
+
+  timeouts {
+    delete = "15m"
+  }
+
+}
+
+
+resource "aws_lb_listener_rule" "web_rule" {
+  listener_arn = data.aws_ssm_parameter.web_alb_listner_rule.value
+  priority     = 10
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web_targetgroup.arn
+  }
+
+  condition {
+    host_header {
+      values = ["${var.tags.Component}-${var.Environment}.${var.zone_name}"]
+    }
+  }
+}
+
+
+resource "aws_autoscaling_policy" "cpu_target_tracking" {
+  name                   = "${local.name}-${var.tags.Component}-autoscalinggrp-tracking-policy"
+  autoscaling_group_name = aws_autoscaling_group.web_autoscaling.name
+  policy_type            = "TargetTrackingScaling"
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 5.0
+  }
+}
