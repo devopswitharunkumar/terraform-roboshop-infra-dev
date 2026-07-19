e88696e443d32edeb06e342ae7fbd1e5acf7e1e7
arunkumar261 <arunkumar261@gmail.com>
2026-07-19 16:09:26 +0530

    we alb module code

--- 08-web-alb/parameter.tf
+++ 08-web-alb/parameter.tf
@@ -3,3 +3,9 @@ resource "aws_ssm_parameter" "web_alb_listner_rule" {
   type  = "String"
   value = aws_lb_listener.https.arn
 }
+
+resource "aws_ssm_parameter" "web_alb_dns_name" {
+  name  = "/${var.Project_Name}/${var.Environment}/web_alb_dns_name"
+  type  = "String"
+  value = aws_lb.web_alb.dns_name
+}
