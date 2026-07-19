052b5c7eb28b1e66ad2cabdcef36e41f062e2068
arunkumar261 <arunkumar261@gmail.com>
2026-07-19 16:40:57 +0530

    updated catalogue syntax errorcode

--- 06-catalogue/locals.tf
+++ 06-catalogue/locals.tf
@@ -1,6 +1,6 @@
 locals {
   name = "${var.Project_Name}-${var.Environment}"
   current_date = formatdate("YYYY-MM-DD-hh-mm", timestamp())
-  private_subnet_id = element(split(",", data.aws_ssm_parameter.private_subnet_id), 0)
+  private_subnet_id = element(split(",", data.aws_ssm_parameter.private_subnet_id.value), 0)
 }
 
